7a71a094189d515d71638ff572511c621c23c57f
ldynia <l_dynowski@yahoo.com>
2020-10-16 12:06:35 +0200

    working on example-00

--- code/terraform/00-preface/hello-world/main.tf
+++ code/terraform/00-preface/hello-world/main.tf
@@ -1,16 +1,33 @@
+# links: https://www.terraform.io/docs/configuration/resources.html
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  required_version = ">= 0.13, < 0.14"
 }
 
 provider "aws" {
   region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
 }
 
+variable ect_instances_count {
+  description = "Number of EC2 instances"
+  type        = number
+  default     = 1
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  count = var.ect_instances_count
+  
+  tags = {
+    Name = "hello world ${count.index}"
+    Environment = "development"
+    Key = "value"
+  }
+  
+  lifecycle {
+    # prevent_destroy = true
+    prevent_destroy = false
+  } 
 }
 
